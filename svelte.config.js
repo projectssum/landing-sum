dfcb166177fcf4fac6800d390e71cc87f786b6dd
unknown <nata-mother-hamster@mail.ru>
2023-12-23 11:03:50 +0300

    install

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter(),
         paths: {
-            base: '/landing-sum'
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
         }
     }
 };
