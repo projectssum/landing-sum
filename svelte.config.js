02dcc6732d864517086342c5c730bcecbecf2868
unknown <nata-mother-hamster@mail.ru>
2024-01-21 10:57:49 +0300

    update

--- svelte.config.js
+++ svelte.config.js
@@ -18,3 +18,25 @@ const config = {
 };
 
 export default config;
+import adapter from "@sveltejs/adapter-static";
+// was "@sveltejs/adapter-auto"
+
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
+const config = {
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? """ : "/your-repo-name",
+        },
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: "#svelte"
+    }
+};
+
+export default config;
