d8fb70b44260a09517760c4d1b510e2928b1d6f9
unknown <nata-mother-hamster@mail.ru>
2024-01-27 00:55:51 +0300

    update

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter(),
         paths: {
-            base: process.argv.includes('dev') ? '' : '/landing-sum'
+            base: '/landing-sum'
         }
     }
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	base: "/landing-sum",
 	plugins: [sveltekit()]
+
 });
